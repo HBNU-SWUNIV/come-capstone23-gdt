b1fe14b270252d0b5766e88d2d3733ad02838da0
hamellen <zzodong0309@naver.com>
2023-04-12 15:06:43 +0900

    230412-4

--- 003 Code/Miracle/Assets/Scenes/Movement2D.cs
+++ 003 Code/Miracle/Assets/Scenes/Movement2D.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 
@@ -14,7 +15,12 @@ public class Movement2D : MonoBehaviour
     private float jumpforce = 8.0f;
 
     private Rigidbody2D rigid2d;
- 
+
+    [SerializeField]
+    private LayerMask groundLayer;
+    private CapsuleCollider2D capsulecollider2d;
+    private bool isGround;
+    private Vector3 footposition;
 
 
 
@@ -22,8 +28,18 @@ public class Movement2D : MonoBehaviour
     private void Awake()
     {
         rigid2d=GetComponent<Rigidbody2D>();
-       
+        capsulecollider2d = GetComponent<CapsuleCollider2D>();
     }
+
+    private void FixedUpdate()
+    {
+        Bounds bounds = capsulecollider2d.bounds;
+        footposition = new Vector2(bounds.center.x, bounds.min.y);
+        isGround = Physics2D.OverlapCircle(footposition, 0.1f, groundLayer);
+
+    }
+
+
 
     public void Move(float x)
     {
@@ -32,7 +48,9 @@ public class Movement2D : MonoBehaviour
 
     public void Jump() {
 
-
-        rigid2d.velocity = Vector2.up * jumpforce;
+        if (isGround == true) {
+            rigid2d.velocity = Vector2.up * jumpforce;
+        }
+        
     }
 }
