619d927bc260f83751f41d6754e6004d9acf83da
hamellen <zzodong0309@naver.com>
2023-04-17 22:27:07 +0900

    230417-2

--- 003 Code/Miracle/Assets/Scenes/C#/EnemyMove.cs
+++ 003 Code/Miracle/Assets/Scenes/C#/EnemyMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     Rigidbody2D rigid;
+    SpriteRenderer sprite;
 
     [SerializeField]
     public int movespeed;
@@ -15,7 +16,7 @@ public class EnemyMove : MonoBehaviour
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-
+        sprite = GetComponent<SpriteRenderer>();
         
         Invoke("Think", 5);
     }
@@ -26,13 +27,12 @@ public class EnemyMove : MonoBehaviour
         rigid.velocity = new Vector2(nextMove*movespeed,rigid.velocity.y);
 
 
-        Vector2 frontVec = new Vector2(rigid.position.x + nextMove, rigid.position.y);
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.2f, rigid.position.y);
         Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("ground"));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("ground"));
         if (rayHit.collider == null)
         {
-
-
+            Turn();
         }
     }
 
@@ -40,6 +40,21 @@ public class EnemyMove : MonoBehaviour
 
         nextMove = Random.Range(-1, 2);
 
+        //방향전환
+        if (nextMove != 0) {
+            sprite.flipX = (nextMove == 1);
+        }
+
+        float nextThinktime = Random.Range(2f, 5f);
+
+        Invoke("Think", nextThinktime);
+    }
+
+    void Turn() {
+
+        nextMove *= -1;
+        sprite.flipX = (nextMove == 1);
+        CancelInvoke();
         Invoke("Think", 5);
     }
 }
