0980536b129aef3f2ac12082b20c14dbdeb8aa75
hamellen <zzodong0309@naver.com>
2023-04-19 23:31:13 +0900

    230419-1
    creating status

--- 003 Code/Miracle/Assets/Scenes/C#/Status.cs
+++ 003 Code/Miracle/Assets/Scenes/C#/Status.cs
@@ -5,18 +5,13 @@ using UnityEngine;
 
 public class Status : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Player_Status status = new Player_Status(100, 20, 30, 5, 0, 10, 0);
-    }
-}
-struct Player_Status 
-{
 
-    private int hp, offensive_power, defensive_power, damage, move_speed, protective_film, attack_speed, critical;
 
-    public Player_Status(int hp, int offensive_power, int defensive_power, int move_speed, int protective_film, int attack_speed, int critical) {//체력,공격력,방어력,데미지,이동속도,실드,공격속도,치명타율
+    private int hp, offensive_power, defensive_power, move_speed, protective_film, attack_speed, critical;
+    
+
+    public Status(int hp, int offensive_power, int defensive_power, int move_speed, int protective_film, int attack_speed, int critical)
+    {//체력,공격력,방어력,데미지,이동속도,실드,공격속도,치명타율
 
         this.hp = hp;
         this.offensive_power = offensive_power;
@@ -25,9 +20,59 @@ struct Player_Status
         this.protective_film = protective_film;
         this.attack_speed = attack_speed;
         this.critical = critical;
-
-
     }
 
-
+    public void add_hp(int add)
+    {
+        this.hp += add;
+    }
+    public void add_offensive_power()
+    {
+        this.offensive_power += 10;
+    }
+    public void init_offensive_power()
+    {
+        this.offensive_power =20;
+    }
+    public void add_defensive_power()
+    {
+        this.defensive_power += 10;
+    }
+    public void init_defensive_power()
+    {
+        this.offensive_power = 30;
+    }
+    public void add_move_speed()
+    {
+        this.move_speed += 10;
+    }
+    public void init_move_speed()
+    {
+        this.move_speed = 5;
+    }
+    public void add_protective_film()
+    {
+        this.protective_film += 10;
+    }
+    public void add_attack_speed()
+    {
+        this.attack_speed += 10;
+    }
+    public void init_attack_speed()
+    {
+        this.attack_speed = 10;
+    }
+    public void add_critical()
+    {
+        this.critical += 10;
+    }
+    public void init_critical()
+    {
+        this.critical = 0;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Status status = new Status(100, 20, 30, 5, 0, 10, 0);
+    }
 }
--- 003 Code/Miracle/Assets/Scenes/C#/playercontroller.cs
+++ 003 Code/Miracle/Assets/Scenes/C#/playercontroller.cs
@@ -1,19 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class playercontroller : MonoBehaviour
 {
-
+    public enum goodstate { non,strength, quick ,solid, agility, focus ,recovery};//±«·Â,½Å¼Ó,°ß°í,¹ÎÃ¸,ÁýÁß,È¸º¹
+    public enum badstate { non,burn,weak, deceleration ,destroy,toxin,coldair,cooling}//È­»ó,¾àÈ­,°¨¼Ó,ÆÄ±«,µ¶,³Ã±â,³Ã°¢
     private Movement2D movement2d;
-    
+    private Status status;
+    public goodstate new_goodstate = goodstate.non;
+    public goodstate current_goodstate = goodstate.non;
+    int currentnumber_goodstate = 0;
+    int maxnumber_goodstate = 3;
     // Start is called before the first frame update
     private void Awake()
     {
         movement2d = GetComponent<Movement2D>();
-        
+        status= GetComponent<Status>();
     }
+    public void Get_HP(int add)
+    {
+        status.add_hp(add);
+    }
+    
+    public void Apply_goodstate() 
+    {
+        switch (new_goodstate)
+        {
+            case goodstate.non:
+                break;
+            case goodstate.strength:
 
+                current_goodstate = goodstate.strength;
+                if (currentnumber_goodstate != maxnumber_goodstate)
+                {
+                    status.add_offensive_power();
+                    currentnumber_goodstate++;
+                }
+                break;
+            case goodstate.quick:
+                current_goodstate = goodstate.quick;
+                if (currentnumber_goodstate != maxnumber_goodstate)
+                {
+                    status.add_attack_speed();
+                    currentnumber_goodstate++;
+                }
+                break;
+            case goodstate.solid:
+                current_goodstate = goodstate.solid;
+                if (currentnumber_goodstate != maxnumber_goodstate)
+                {
+                    status.add_defensive_power();
+                    currentnumber_goodstate++;
+                }
+                break;
+            case goodstate.agility:
+                current_goodstate = goodstate.agility;
+                if (currentnumber_goodstate != maxnumber_goodstate)
+                {
+                    status.add_move_speed();
+                    currentnumber_goodstate++;
+                }
+                break;
+            case goodstate.focus:
+                current_goodstate = goodstate.focus;
+                if (currentnumber_goodstate != maxnumber_goodstate)
+                {
+                    status.add_critical();
+                    currentnumber_goodstate++;
+                }
+                break;
+            case goodstate.recovery:
+                status.add_hp(30);
+                break;
+        }
+    }
+    public void init_goodstate(goodstate state)
+    {
+        switch (state)
+        {
+            
+
+
+
+
+        }
+
+    }
     // Update is called once per frame
     void Update()
     {
